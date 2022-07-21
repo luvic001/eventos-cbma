6f68d5a89deb2f472a75e617271f4f9c7762e320
lucasvictor <lucas.v0697@gmail.com>
2022-07-21 14:20:47 -0300

    ADD footer

--- modules/page/global/Footer.php
+++ modules/page/global/Footer.php
@@ -6,6 +6,58 @@ if (!defined('PATH')) exit;
 
 <footer class="site-footer">
 
-  
+  <div class="container-fluid">
+    <div class="site-logo">
+      <a href="http://2022.congressodearbitragem.cbma.local.com.br" class="no-decoration" title="RJAW | RIO ARBITRATION WEEK">
+        <h2><b>RJ</b>AW | RIO ARBITRATION WEEK</h2>
+      </a>
+    </div>
+    <div class="d-flex flex-lg-row flex-column align-items-lg-end align-items-center justify-content-lg-between">
+
+      <div class="copyrights info-footer">
+        <p>© 2022. Todos os direitos reservados</p>
+      </div>
+
+      <div class="redes-sociais info-footer">
+        <ul>
+          <li>
+            <a href="#" target="_blank">
+              <i class="fab fa-2x fa-facebook-square"></i>
+            </a>
+          </li>
+          <li>
+            <a href="#" target="_blank">
+              <i class="fab fa-2x fa-instagram"></i>
+            </a>
+          </li>
+          <li>
+            <a href="#" target="_blank">
+              <i class="fab fa-2x fa-linkedin-in"></i>
+            </a>
+          </li>
+          <li>
+            <a href="#" target="_blank">
+              <i class="fab fa-2x fa-twitter"></i>
+            </a>
+          </li>
+          <li>
+            <a href="#" target="_blank">
+              <i class="fab fa-2x fa-youtube"></i>
+            </a>
+          </li>
+        </ul>
+      </div>
+
+      <div class="develop info-footer">
+        <p>
+          Desenvolvido por: <br/>
+          <a href="https://agenciacampana.com.br" target="_blank" title="Agência Campana | Marketing Digital">
+            <img src="<?= get_image('logo-campana.png') ?>" alt="Agência Campana">
+          </a>
+        </p>
+      </div>
+
+    </div>
+  </div>
 
 </footer>
