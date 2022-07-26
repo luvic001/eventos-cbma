9f41294fefe572f111887025852e0a364ecb39bb
lucasvictor <lucas.v0697@gmail.com>
2022-07-26 10:29:25 -0300

    UPDATE event_list - ADD (Global Experience...)

--- modules/page/Home/Calendario-de-eventos/Eventos/Lista-de-eventos.php
+++ modules/page/Home/Calendario-de-eventos/Eventos/Lista-de-eventos.php
@@ -34,8 +34,8 @@ $eventos = [
   'Agosto de 2022' => [
     [
       'nome_do_evento' => 'Coquetel Mulher',
-      'data_do_evento_inicial' => '2022-08-09', 
-      'data_do_evento_final' => '2022-08-09', 
+      'data_do_evento_inicial' => '2022-08-08', 
+      'data_do_evento_final' => '2022-08-08', 
       'horario_do_evento_inicial' => '18:30:00',
       'horario_do_evento_final' => '21:00:00',
       'local' => 'Kincaid Advogados <br/> Av. Rio Branco 1, 14º andar',
@@ -43,6 +43,102 @@ $eventos = [
       'link_do_cadastro' => false,
       'informacoes_adicionais' => false,
       'precos' => false
-    ]
+    ],
+    [
+      'nome_do_evento' => 'Abertura do Curso Prático de Arbitragem Esportiva',
+      'data_do_evento_inicial' => '2022-08-08', 
+      'data_do_evento_final' => '2022-08-08', 
+      'horario_do_evento_inicial' => '19:00:00',
+      'horario_do_evento_final' => '21:00:00',
+      'local' => false,
+      'link_do_evento' => 'https://youtube.com/c/CPACursoPraticodeArbitragem',
+      'link_do_cadastro' => false,
+      'informacoes_adicionais' => false,
+      'precos' => false
+    ],
+    [
+      'nome_do_evento' => 'Arbitragem e Direito Marítimo',
+      'data_do_evento_inicial' => '2022-08-09', 
+      'data_do_evento_final' => '2022-08-09', 
+      'horario_do_evento_inicial' => '09:30:00',
+      'horario_do_evento_final' => '11:30:00',
+      'local' => 'Kincaid Advogados <br/> Av. Rio Branco 1, 14º andar',
+      'link_do_evento' => false,
+      'link_do_cadastro' => false,
+      'informacoes_adicionais' => false,
+      'precos' => false
+    ],
+    [
+      'nome_do_evento' => 'Arbitragem, Blockchain, Criptomoeda e Metaverso',
+      'data_do_evento_inicial' => '2022-08-09', 
+      'data_do_evento_final' => '2022-08-09', 
+      'horario_do_evento_inicial' => '18:00:00',
+      'horario_do_evento_final' => '20:00:00',
+      'local' => 'OAB-RJ <br/> Av. Marechal Câmara 150, 4º andar',
+      'link_do_evento' => false,
+      'link_do_cadastro' => false,
+      'informacoes_adicionais' => false,
+      'precos' => false
+    ],
+    [
+      'nome_do_evento' => 'Evento do CJA',
+      'data_do_evento_inicial' => '2022-08-09', 
+      'data_do_evento_final' => '2022-08-09', 
+      'horario_do_evento_inicial' => '18:00:00',
+      'horario_do_evento_final' => '23:00:00',
+      'local' => 'CBMA <br/> Rua da Candelária, 9/803',
+      'link_do_evento' => false,
+      'link_do_cadastro' => false,
+      'informacoes_adicionais' => false,
+      'precos' => false
+    ],
+    [
+      'nome_do_evento' => 'Global Experience in Regulatory Sandboxes: Current Challenges and Future Perspectives',
+      'data_do_evento_inicial' => '2022-08-10', 
+      'data_do_evento_final' => '2022-08-10', 
+      'horario_do_evento_inicial' => '09:00:00',
+      'horario_do_evento_final' => '10:30:00',
+      'local' => false,
+      'link_do_evento' => 'https://www.youtube.com/c/CBMAoficial',
+      'link_do_cadastro' => false,
+      'informacoes_adicionais' => 'Local: International Webinar - Evento Online',
+      'precos' => false
+    ],
+    [
+      'nome_do_evento' => 'Course on CISG and International Contracts',
+      'data_do_evento_inicial' => '2022-08-10', 
+      'data_do_evento_final' => '2022-08-10', 
+      'horario_do_evento_inicial' => '18:00:00',
+      'horario_do_evento_final' => '23:00:00',
+      'local' => 'CBMA <br/> Rua da Candelária, 9/803',
+      'link_do_evento' => false,
+      'link_do_cadastro' => false,
+      'informacoes_adicionais' => false,
+      'precos' => false
+    ],
+    [
+      'nome_do_evento' => 'Congresso Internacional CBMA de Arbitragem',
+      'data_do_evento_inicial' => '2022-08-11', 
+      'data_do_evento_final' => '2022-08-12', 
+      'horario_do_evento_inicial' => '00:00:00',
+      'horario_do_evento_final' => '23:59:59',
+      'local' => 'Museu do Amanhã <br/> Praça Mauá, nº 1, Centro',
+      'link_do_evento' => false,
+      'link_do_cadastro' => false,
+      'informacoes_adicionais' => 'O dia todo',
+      'precos' => false
+    ],
+    [
+      'nome_do_evento' => 'Evento ICC YAF/CJA/CAMARB: Simulação do Caso da Competição Brasileira de Arbitragem',
+      'data_do_evento_inicial' => '2022-08-12', 
+      'data_do_evento_final' => '2022-08-12', 
+      'horario_do_evento_inicial' => '16:00:00',
+      'horario_do_evento_final' => '18:00:00',
+      'local' => 'Museu do Amanhã <br/> Praça Mauá, nº 1, Centro',
+      'link_do_evento' => false,
+      'link_do_cadastro' => false,
+      'informacoes_adicionais' => 'O dia todo',
+      'precos' => false
+    ],
   ]
 ];
